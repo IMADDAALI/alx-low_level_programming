193425c40fff55280ae4425fe00cc0fa8a79be03
IMADDAALI <imaddaali2@gmail.com>
2023-10-19 12:48:00 -0700

    az

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,23 +7,19 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t nelem = 0;
+	size_t nelem = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-        {
-            printf("[%d] %s\n", 0, "(nil)");
-        }
-        else
-        {
-            printf("[%d] %s\n", h->len, h->str);
-        }
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-        h = h->next;
-        nelem++;
-    }
+		h = h->next;
+		nelem++;
+	}
 
-    return nelem;
+	return (nelem);
 }
 
