84729da73859117eb88ef8095ec4b91990ae20f0
IMADDAALI <imaddaali2@gmail.com>
2023-10-19 16:10:23 -0700

    az

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,14 +7,14 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t nelem = 0;
+	size_t nelem = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        nelem++;
-    }
+	while (h != NULL)
+	{
+		h = h->next;
+		nelem++;
+	}
 
-    return nelem;
+	return (nelem);
 }
 
