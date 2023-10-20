79e6f8653b8baa1aa487b65960a5e6245b5190b1
IMADDAALI <imaddaali2@gmail.com>
2023-10-19 17:03:33 -0700

    az

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,20 +2,11 @@
 
 /**
  * pre_main - Function executed before main.
- *
- * Description: This function is executed before the main function.
- * It prints a message.
+ * Return: no return.
  */
+
 void __attribute__ ((constructor)) pre_main()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
 }
-
-int main(void)
-{
-	/* Your main function code here */
-
-	return (0);
-}
-
