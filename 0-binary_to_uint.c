b4f57329ecf1b9d92af42b811366c0efaab6fe0d
IMADDAALI <imaddaali2@gmail.com>
2023-10-26 15:20:09 -0700

    alx

---
+++ 0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int result;
+    int length, weight;
+
+    if (!b)
+        return (0);
+
+    result = 0;
+
+    for (length = 0; b[length] != '\0'; length++)
+        ;
+
+    for (length--, weight = 1; length >= 0; length--, weight *= 2)
+    {
+        if (b[length] != '0' && b[length] != '1')
+        {
+            return (0);
+        }
+
+        if (b[length] & 1)
+        {
+            result += weight;
+        }
+    }
+
+    return (result);
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-README FILE
+0x14. C - Bit manipulation
