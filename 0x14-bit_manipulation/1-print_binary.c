2420ad8c8e4f0a43cf11477c53ea3768ef34c2e0
IMADDAALI <imaddaali2@gmail.com>
2023-10-26 15:54:40 -0700

    alx

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,17 +3,17 @@
 /**
  * print_binary - prints the binary representation
  * of a number.
- * @n: unsigned long int.
+ * @n: unsigne to edit long int.
  *
  * Return: no return.
  */
 void print_binary(unsigned long int n)
 {
-	if (num >> 0)
+	if (n >> 0)
 	{
-		if (num >> 1)
-			print_binary(num >> 1);
-		_putchar((num & 1) + '0');
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
 	else
 	{
